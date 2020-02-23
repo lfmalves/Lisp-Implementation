e8076c926cca4fac780e058206025632bfa550e9
lfmalves <luis.projensino@gmail.com>
2020-02-23 14:46:50 -0300

    initial parsers for basic polish notation

--- parsing.c
+++ parsing.c
@@ -5,6 +5,20 @@
 #include <editline/history.h>
 
 int main(int argc, char** argv) {
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* Poorlisp = mpc_new("poorlisp");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+  "                                                     \
+    number   : /-?[0-9]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' ;                  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    lispy    : /^/ <operator> <expr>+ /$/ ;             \
+  ",
+            Number, Operator, Expr, Poorlisp);
+
   puts("PoorLisp Version 0.0.1");
   puts("Press Ctrl+c to Exit\n");
 
@@ -14,5 +28,7 @@ int main(int argc, char** argv) {
     printf("Spitting back %s\n", input);
     free(input);
   }
+
+  mpc_cleanup(4, Number, Operator, Expr, Poorlisp);
   return 0;
 }
