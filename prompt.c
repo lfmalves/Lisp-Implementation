b25dcd67e64dcc93f5fa98eaef62f4561babd78c
lfmalves <luis.projensino@gmail.com>
2020-02-23 13:33:03 -0300

    linux adaptations!

--- prompt.c
+++ prompt.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+#include <editline/readline.h>
+#include <editline/history.h>
 
 int main(int argc, char** argv) {
   puts("PoorLisp Version 0.0.1");
   puts("Press Ctrl+c to Exit\n");
 
   while (1) {
-    fputs("poorlisp> ", stdout);
-    fgets(input, 2048, stdin);
+    char* input = readline("poorlisp> ");
+    add_history(input);
     printf("Spitting back %s", input);
+    free(input);
   }
   return 0;
 }
