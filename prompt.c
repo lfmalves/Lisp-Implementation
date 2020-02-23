2a5901365b2de5ff48da4e7b0985f75e9bed10b9
lfmalves <luis.projensino@gmail.com>
2020-02-23 13:40:14 -0300

    up dowm repl search implemented

--- prompt.c
+++ prompt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <editline/readline.h>
+#include <editline/readline.h> //sudo apt-get install libedit-dev
 #include <editline/history.h>
 
 int main(int argc, char** argv) {
@@ -11,7 +11,7 @@ int main(int argc, char** argv) {
   while (1) {
     char* input = readline("poorlisp> ");
     add_history(input);
-    printf("Spitting back %s", input);
+    printf("Spitting back %s\n", input);
     free(input);
   }
   return 0;
