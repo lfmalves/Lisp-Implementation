83c4d051d38a3a7a2b6e036503f583b3b0f24e7e
lfmalves <luis.projensino@gmail.com>
2020-02-25 14:54:12 -0300

    lval_add pointer and lval_del func

--- parsing.c
+++ parsing.c
@@ -57,6 +57,28 @@ lval* lval_sexpr(void) {
   return v;
 }
 
+void lval_del(lval* v) {
+  switch (v->type) {
+  case LVAL_NUM: break;
+  case LVAL_ERR: free(v->err); break;
+  case LVAL_SYM: free(v->sym); break;
+  case LVAL_SEXPR:
+    for (int i = 0; i < v->count; i++) {
+      lval_del(v->cell[i]);
+    }
+    free(v->cell);
+    break;
+  }
+  free(v);
+}
+
+lval* lval_add(lval* v, lval* x) {
+  v->count++;
+  v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+  v->cell[v->count-1] = x;
+  return v;
+}
+
 void lval_print(lval v) {
   switch (v.type) {
   case LVAL_NUM: printf("%li", v.num); break;
