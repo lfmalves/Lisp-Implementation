b22eb1692819d3424359432bfca17f2ac6930084
lfmalves <luis.projensino@gmail.com>
2020-02-25 15:26:34 -0300

    lval_eval_sexpr and lval_eval

--- parsing.c
+++ parsing.c
@@ -146,6 +146,32 @@ lval* builtin_op(lval* a, char* op) {
   return x;
 }
 
+lval* lval_eval(lval* v);
+
+lval* lval_eval_sexpr(lval* v) {
+  for (int i = 0; i < v->count; i++) {
+    v->cell[i] = lval_eval(v->cell[i]);
+  }
+  for (int i = 0; i < v->count; i++) {
+    if (v->cell[i]->type == LVAL_ERR){ return lval_take(v, i); }
+  }
+  if (v->count == 0) { return v; }
+  if (v->count == 1) { return lval_take(v, 0); }
+  lval* f = lval_pop(v, 0);
+  if (f->type != LVAL_SYM) {
+    lval_del(f); lval_del(v);
+    return lval_err("S-expression does not start with symbol");
+  }
+  lval* result = builtin_op(v, f->sym);
+  lval_del(f);
+  return result;
+}
+
+lval* lval_eval(lval* v) {
+  if (v->type == LVAL_SEXPR) { return lval_eval_sexpr(v); }
+  return v;
+}
+
 lval eval(mpc_ast_t* t) {
   if (strstr(t->tag, "number")) {
     errno = 0;
